83efa55670c4e87e03b33b9cfa8ac03fea2fad3b
18041609 <18041609@suning.com>
2021-04-07 10:50:05 +0800

    提交一个新的项目

---
+++ github-test/src/main/java/com/eggg/githubtest/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.eggg.githubtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈功能详细描述〉
+ *
+ * @author 18041609
+ * @date 2021/4/7
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello world";
+    }
+
+}
